389095398eb94e8be5d132876efbc6d2e1e24383
Devair Luiz Moitim <dmoitim@gmail.com>
2018-12-23 10:31:54 -0200

    Configuração do webpack e hello world

---
+++ front-end/webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        publicPath: '/',
+        contentBase: './dist',
+        port: 80
+    }
+};
